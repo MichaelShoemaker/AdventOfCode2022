0d28ed1bceccb01a277b3b56aed2c7ed6d9d8f57
MichaelShoemaker <mikeshoemaker2000@yahoo.com>
2022-12-08 15:18:35 -0600

    Finshed Part1

--- Day8/runner.py
+++ Day8/runner.py
@@ -2,7 +2,7 @@ from time import sleep
 
 def check_heights(s: list, i: int, t: int) -> bool:
     for l in s:
-        if int(l[i]) > t:
+        if int(l[i]) >= t:
             return False
     return True
 
@@ -14,26 +14,32 @@ def make_grid(file:str):
 
     height = len(grid)
     width = len(grid[0])
-
+    #print(grid)
     visible += (height * 2) + (width * 2) - 4
     for h in range(1,len(grid)-1):
         for w in range(1,len(grid[0])-1):
+            if h == 97 and w > 94:
+                print(grid[h][w])
             tree = int(grid[h][w])
             if all(int(x) < tree for x in grid[h][:w]):
-                print(h,w, grid[h][w],'a')
+                if h == 97 and w > 94:
+                    print(h,w, grid[h][w],'a')
                 visible += 1
                 continue
             if all(int(x) < tree for x in grid[h][w+1:]):
-                print(h,w, grid[h][w],'b')
+                if h == 97 and w > 94:
+                    print(h,w, grid[h][w],'b')
                 visible += 1
                 continue
             if check_heights(grid[:h], w, tree):
                 # print(check_heights(grid[:h], w, tree))
-                print(h,w, grid[h][w],'c')
+                if h == 97 and w > 94:
+                    print(h,w, grid[h][w],'c')
                 visible +=1 
                 continue
             if check_heights(grid[h+1:], w, tree):
-                print(h,w, grid[h][w],'d')
+                if h == 97 and w > 94:
+                    print(h,w, grid[h+1:][0][95:],'d')
                 visible +=1
                 continue
 
